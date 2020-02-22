b820e73155035ee00e5319a9ded819afbc993a1c
Alain Nguyen <onealainn@gmail.com>
2020-02-21 22:37:58 -0500

    Removed sensitive data

--- config.js
+++
@@ -1,3 +0,0 @@
-module.exports.mongoDB = "mongodb+srv://aloin:261178@cluster0-pfuao.mongodb.net/test?retryWrites=true&w=majority"
-module.exports.csrf_key = '4#yr..~%hN%o17oJ-jg%"$-)W#V1%Z2XLZzd6z8maI^mA]*]2t.FyklY8(!KM';
-module.exports.secret = "LOL"
